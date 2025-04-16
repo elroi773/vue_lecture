5a04de47e007932e0e5e7cb39b9e4c6dd0ad2441
elroi773 <s2457@e-mirim.hs.kr>
2025-04-17 06:52:58 +0900

    Reset tracking with proper .gitignore

--- App.vue
+++ App.vue
@@ -1,26 +1,43 @@
-<script>
-export default {
-  data () { 
-      // data :function 이것도 가능하기는 함
-      return {
-        name : '철수',
-        age: 19,
-        message:'<h1>Hello, Vue!</h1>',
-      };
-    }
-}
-</script>
 <template>
-  <h1>안녕하세용 {{ name }} / {{ age }}</h1>
-  <!-- 콧수염 문법 vue 의 변수를 가지고 올 수 있음  -->
+  <div id="app">
+    <div class="todo">
+      <div class="todo_title">
+        <h1 class="todo_text">Todo List</h1>
+        <ul class="todo_tab">
+          <li class="todo_tab--active">전체</li>
+          <li>완료</li>
+        </ul>
+      </div>
+      <div class="todo_list">
+        <div class="todo_item">
+          <input type="checkbox" id = "chk">
+          <label for="chk" class="todo_checkgox-label"></label>
+          <span class="todo_item-text">아침 먹기</span>
+          <span class="material-symbols-outlined todo_delete-icon">delete</span>
+        </div>
+        <div class="todo_item--no" style="display: none;">
+          <p>할 일이 없습니다</p>
+        </div>
+      </div>
+      <div class="todo_input">
+        <input type="text" class="todo_input-text" placeholder="할일을 입력하세요">
+        <button class = "todo_input-btn">등록</button>
+      </div>
+    </div>
+  </div>
+</template>
 
-  <h1 v-html="name"></h1>
-  <h1 v-text="name"></h1> 
-  <!-- v-html 은 html 태그로 랜더링 하지만 text 는 문자열로 처리  -->
-  <div v-html="message"></div>
-  <div v-text="message"></div>
+<script>
 
-</template> 
-<!-- html 의 바디느낌  -->
-<style></style>
-<!-- vue 의 기본 구조  -->
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
