8b267b5fb75da66d2c685aba1bea3bb118307fa7
elroi773 <s2457@e-mirim.hs.kr>
2025-04-17 06:52:16 +0900

    Fix .gitignore to ignore folders

--- 02/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import FirstChild  from "./componets/FirstChild.vue";
-const app = (App);
-app.componet("FirstChild",FirstChild);
-app.mount('#app')
