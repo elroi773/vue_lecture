a1781e8a1d4b6326de84f2dcda9e8982314883b0
elroi773 <s2457@e-mirim.hs.kr>
2025-04-25 00:06:00 +0900

    pinia

---
+++ pinia-demo/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useCountStore } from '@/stores/count'
+
+const countStore = useCountStore()
+</script>
+
+<template>
+  <div>
+    <h1>Num: {{ countStore.num }}</h1>
+    <h2>Double Num: {{ countStore.doubleNum }}</h2>
+    <button @click="countStore.increment">+</button>
+  </div>
+</template>
