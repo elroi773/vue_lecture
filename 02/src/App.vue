bd69f648db328d7d10589ff36c8aefe6dd7c8127
elroi773 <s2457@e-mirim.hs.kr>
2025-05-11 22:20:52 +0900

    .

---
+++ 02/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>App</h1>
+  <FirstChild/>
+</template>
+
+<script>
+import FirstChild from '@/components/FirstChild.vue'
+export default {
+  components : {
+    FirstChild
+  },
+  provide() { // ①
+    return {
+      message: this.message,
+    };
+  },
+  data() {
+    return {
+      message: 'Hello from Vue JS!',
+    };
+  },
+}
+
+</script>
+
+<style></style>
