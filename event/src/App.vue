f32c27a9b9ed52de0844a29814f09ec98da52401
elroi773 <s2457@e-mirim.hs.kr>
2025-05-15 22:26:05 +0900

    event

--- event/src/App.vue
+++ event/src/App.vue
@@ -9,12 +9,16 @@ export default{
     increment(){
       this.number++;
     },
+    increment(){
+      this.number2++;
+    },
     
   },
 };
 </script>
 <template>
-  <h1>{{ number }}</h1>
-  <button @click="increment"> 증가 </button>
+  <h1 v-memo="[number, number2]">{{ number }} / {{ number2 }}</h1>
+  <button @click.once="increment"> 증가 </button> 
+  <!-- 이벤트 한번만 -->
 </template>
 <style></style>
