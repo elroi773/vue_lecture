135f88a0560cd8c0900a821c2211371e4058539e
elroi773 <s2457@e-mirim.hs.kr>
2025-05-15 19:54:55 +0900

    event

---
+++ event/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default{
+  data(){
+    return{
+      number:0,
+    };
+  },
+  methods: {
+    increment(){
+      this.number++;
+    },
+    
+  },
+};
+</script>
+<template>
+  <h1>{{ number }}</h1>
+  <button @click="increment"> 증가 </button>
+</template>
+<style></style>
