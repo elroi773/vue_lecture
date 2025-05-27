be40e18eea1baeda1752dc2483455bcf5fcc3ae7
elroi773 <s2457@e-mirim.hs.kr>
2025-05-27 12:16:23 +0900

    if 문

---
+++ 04/src/App.vue
@@ -0,0 +1,27 @@
+
+<script>
+export default{
+  data(){
+    return {
+      visible:true,
+      unvisible:false,
+      condition:"F",
+    };
+  },
+};
+</script>
+
+<template>
+  <p v-if="condition === 'A'">condition{{  condition  }}입니다 </p>
+  <p v-else-if="condition === 'B'">condition{{  condition  }}입니다 </p>
+  <p v-else-if="condition === 'c'">condition{{  condition  }}입니다 </p>
+  <p v-else-if="condition === 'D'">condition{{  condition  }}입니다 </p>
+  <p v-else-if="condition === 'F'">condition{{  condition  }}입니다 </p>
+  <p v-else>condition{{  condition  }}입니다 </p>
+</template>
+
+
+
+<style>
+
+</style>
